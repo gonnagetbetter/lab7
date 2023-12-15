12d300e4d5aded295f7665d39df059aa425053e3
gonnagetbetter <denissss906@gmail.com>
2023-12-15 19:55:30 +0200

    lab7full

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -7,37 +7,37 @@ const Gallery = () => {
     {
       id: 1,
       name: "Apple",
-      image: "",
+      image: "https://github.com/gonnagetbetter/lab7/blob/main/public/img/apple.png?raw=true",
       price: "$1000 each",
     },
     {
       id: 2,
       name: "Cherry",
-      image: "",
+      image: "https://github.com/gonnagetbetter/lab7/blob/main/public/img/cherry.png?raw=true",
       price: "$2.50 per kg",
     },
     {
       id: 3,
       name: "Grape",
-      image: "",
+      image: "https://github.com/gonnagetbetter/lab7/blob/main/public/img/grape.png?raw=true",
       price: "$2.99 per kg",
     },
     {
       id: 4,
       name: "Peach",
-      image: "",
+      image: "https://github.com/gonnagetbetter/lab7/blob/main/public/img/peach.png?raw=true",
       price: "$4.00 per kg",
     },
     {
       id: 5,
       name: "Pear",
-      image: "",
+      image: "https://github.com/gonnagetbetter/lab7/blob/main/public/img/pear.png?raw=true",
       price: "$3.99 per kg",
     },
     {
       id: 6,
       name: "Potato",
-      image: "",
+      image: "https://github.com/gonnagetbetter/lab7/blob/main/public/img/potato.png?raw=true",
       price: "$1.59 per kg",
     },
   ];
